7f3a3f8a389a664015e21701208e5d23485252ba
Mikhail <mikhail.batura@gmail.com>
2025-08-12 18:27:22 +0200

    Paper citation experiment WIP

--- scripts/experiments.py
+++ scripts/experiments.py
@@ -1,4 +1,3 @@
-import sys
 import numpy as np
 from google import genai
 
@@ -26,8 +25,16 @@ def lang_similarity_exp(path, id, client):
 
     print(f"en-ru: {dist_en_ru:.4f}, en-kz: {dist_en_kz:.4f}, ru-kz: {dist_ru_kz:.4f}")
 
+def compare_paper_to_citations(paper_abstract, citatons_abstracts, client):
+    paper_embedding = faiss.get_embedding(paper_abstract, client)
+    for citation_abstract in citatons_abstracts:
+        citation_embedding = faiss.get_embedding(citation_abstract, client)
+        print(cosine_distance(paper_embedding, citation_embedding))
+    print('a')
+
 if __name__ == "__main__":
     gai = genai.Client(api_key=constants.GEMINI_API_KEY)
-    lang_similarity_exp('https://bm.kaznu.kz/index.php/kaznu/', 1608, gai)
-    lang_similarity_exp('https://philart.kaznu.kz/index.php/1-FIL/', 4918, gai)
-    lang_similarity_exp('https://bulletin-psysoc.kaznu.kz/index.php/1-psy/', 2106, gai)
+    # lang_similarity_exp('https://bm.kaznu.kz/index.php/kaznu/', 1608, gai)
+    # lang_similarity_exp('https://philart.kaznu.kz/index.php/1-FIL/', 4918, gai)
+    # lang_similarity_exp('https://bulletin-psysoc.kaznu.kz/index.php/1-psy/', 2106, gai)
+

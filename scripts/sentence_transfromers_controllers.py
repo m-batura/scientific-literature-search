7efd97177d6c3af559c76438658e4c539c1f7a50
m-batura <mikhail.batura@gmail.com>
2026-01-19 17:11:46 +0100

    SentTrans local model
    Added saving and loading, todo lang comparison

--- scripts/sentence_transfromers_controllers.py
+++ scripts/sentence_transfromers_controllers.py
@@ -1,7 +1,14 @@
 from sentence_transformers import SentenceTransformer
 
-model = SentenceTransformer('ibm-granite/granite-embedding-107m-multilingual')
+def save_model(model_name):
+    model = SentenceTransformer(model_name, trust_remote_code=True)
+    model.save('st_models')
 
+def load_model(model_path):
+    return SentenceTransformer(model_path, trust_remote_code=True)
+
+# save_model("Alibaba-NLP/gte-multilingual-base")
+
+model = load_model('st_models')
 query_embedding = model.encode('Small test text')
-
 print(query_embedding[:10])

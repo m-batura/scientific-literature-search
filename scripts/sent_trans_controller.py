16e6dccb803602cba98402b7949138d0bdda8715
m-batura <mikhail.batura@gmail.com>
2026-01-28 17:26:24 +0100

    Optimising with numpy

--- scripts/sent_trans_controller.py
+++ scripts/sent_trans_controller.py
@@ -17,15 +17,18 @@ def cosine_distance(a, b):
 # save_model("Alibaba-NLP/gte-multilingual-base")
 
 if __name__ == "__main__":
+    print('program start')
     time_stamp = time.time()
     model = load_model('gte-multilingual-base')
     print(f'time to load {(time.time() - time_stamp)}')
-    promt = 'automating literature search using web scrapping embeddings and vector search'
-    promt_embedding = model.encode(promt)
+    # promt = 'automating literature search using web scrapping embeddings and vector search'
+    # promt_embedding = model.encode(promt)
     papers = scrap_sinteza.scrap_papers('https://portal.sinteza.singidunum.ac.rs/issue/showAll/2025')
     time_stamp = time.time()
     paper_embeddings = np.array([model.encode(paper[0]) for paper in papers])
+    # paper_similarities = np.array([])
     print(f'avg embed time {(time.time() - time_stamp) / paper_embeddings.shape[0]}')
     time_stamp = time.time()
-    similarities = np.array([cosine_distance(promt_embedding, paper_embedding) for paper_embedding in paper_embeddings])
+    similarities = np.array([cosine_distance(paper_embeddings[0], paper_embedding) for paper_embedding in paper_embeddings])
     print(f'avg distance time {(time.time() - time_stamp) / paper_embeddings.shape[0]}')
+    print(similarities.dtype)

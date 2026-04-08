f52f8e9dab415cdd5e62df96e8b26aa9c81d0835
m-batura <mikhail.batura@gmail.com>
2026-04-08 22:34:01 +0200

    similarity experiment done

--- scripts/testground.py
+++ scripts/testground.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
 
   # print(thisdict['brand', 'model'])
   print('start ')
-  model = SentenceTransformer('C:\\Misha\\University\\FinalThesis\\Automatic-Literature-Review-with-RAG\\st_models\\gte-multilingual-base', trust_remote_code=True)
+  model = SentenceTransformer('C:\\Misha\\University\\FinalThesis\\Automatic-Literature-Review-with-RAG\\st_models\\gte-multilingual-base', trust_remote_code=True, fix_mistral_regex=True)
   print('loaded')
   embeddings = model.encode('Enhancing Retrieval - Augmented Generation with Graph-Based Retrieval and Generative Modeling')  
   print(type(embeddings[0]))

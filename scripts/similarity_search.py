83bdb8ee37232f6a00272933fabfe4669d6bafca
m-batura <mikhail.batura@gmail.com>
2026-04-13 19:42:12 +0200

    blank result for complete solution

--- scripts/similarity_search.py
+++ scripts/similarity_search.py
@@ -24,15 +24,17 @@ def read_titles_embeddings(path=path_to_parquet):
     return pd.read_parquet(path)
 
 def first_retrieval(df, embedding):
-    df['title_rank'] = df['title_embed'].apply(lambda x: stc.cosine_distance(x, embedding))
-    return(df.sort_values(by='title_rank')[['link']].head(10))
+    df['title_dist'] = df['title_embed'].apply(lambda x: stc.cosine_distance(x, embedding))
+    # return(df.loc[df['title_rank'] > 0.31])
+    return(df.sort_values(by='title_dist')[['link', 'title', 'title_dist']].head(10))
 
 def second_retrieval(df, embedding):
     df['abstract'] = df['link'].apply(sinteza.scrap_abstract)
     model = stc.load_model(stc.path_to_gte)
     df['abstract_embed'] = df['abstract'].apply(model.encode)
-    df['abstract_rank'] = df['abstract_embed'].apply(lambda x: stc.cosine_distance(x, embedding))
-    return(df.sort_values(by='abstract_rank')[['link', 'abstract_rank']].head(10))
+    df['abstract_dist'] = df['abstract_embed'].apply(lambda x: stc.cosine_distance(x, embedding))
+    # return(df.loc[df['abstract_dist'] > 0.31])
+    return(df.sort_values(by='abstract_dist')[['link', 'title', 'title_dist', 'abstract', 'abstract_dist']].head(10))
 
 # def find_k_similar_texts(topic, k):
 #     print('get promt embedding')
@@ -50,20 +52,22 @@ def second_retrieval(df, embedding):
 if __name__ == "__main__":
     # save_titles_embeddings()
 
-    # promt = 'This study examines Retrieval-Augmented Generation (RAG) in large language models (LLMs) and their significant application for undertaking systematic literature reviews (SLRs). RAG-based LLMs can potentially automate tasks like data extraction, summarization, and trend identification. However, while LLMs are exceptionally proficient in generating human-like text and interpreting complex linguistic nuances, their dependence on static, pre-trained knowledge can result in inaccuracies and hallucinations. RAG mitigates these limitations by integrating LLMs’ generative capabilities with the precision of real-time information retrieval. We review in detail the three key processes of the RAG framework—retrieval, augmentation, and generation. We then discuss applications of RAG-based LLMs to SLR automation and highlight future research topics, including integration of domain-specific LLMs, multimodal data processing and generation, and utilization of multiple retrieval sources. We propose a framework of RAG-based LLMs for automating SRLs, which covers four stages of SLR process: literature search, literature screening, data extraction, and information synthesis. Future research aims to optimize the interaction between LLM selection, training strategies, RAG techniques, and prompt engineering to implement the proposed framework, with particular emphasis on the retrieval of information from individual scientific papers and the integration of these data to produce outputs addressing various aspects such as current status, existing gaps, and emerging trends.'
+    promt = 'Automating literature search with the help of local embedding models'
     # promt = 'Automating literature search with the help of local embedding models '
     # promt = 'Rastući značaj inovacija u procesu dostizanja održive konkurentne prednosti je u najvećoj meri i uslovio ubrzani razvoj internet marketinga i inicirao sve veću upotrebu društvenih mreža u cilju unapređenja dugoročnog odnosa organizacije i ciljne grupe potrošača. U novoj ekonomiji, odnosno ekonomiji znanja, sam pojam inovacija, pored značajnih unapređenja proizvoda i procesa, sve više podrazumeva organizacione i marketing inovacije, a naročito inovacije u oblasti internet marketinga. Usled navedenih trendova, u ovom naučnom radu, posebna pažnja je usmerena upravo na analizu uticaja koji internet marketing i upotreba internet društvenih mreža imaju na proces unapređenja dvosmerne, odnosno interaktivne komunikacije sa potrošačima. Analizom su obuhvaćeni i brojni primeri realizovanih istraživanja čiji dobijeni rezultati ukazuju da upravo inovacije u oblasti internet marketinga postaju krucijalni faktor direferenciranja i dostizanja održive konkurentne prednosti organizacija, ali i ukazuju kakvo je trenutno stanje u oblasti internet marketinga i upotrebe internet društvenih mreža u Republici Srbiji.'
-    # model = stc.load_model(stc.path_to_gte)
-    # embedding = model.encode(promt)
-    # df = read_titles_embeddings()
-    # print('got df')
-    # df_first = first_retrieval(df, embedding)
-    # print('got titles')
-    # df_second = second_retrieval(df_first, embedding)
-    # print(df_second.to_string())
-
+    
+    model = stc.load_model(stc.path_to_gte)
+    embedding = model.encode(promt)
     df = read_titles_embeddings()
-    print(df.iloc[0]['title_embed'].dtype)
+    df_first = first_retrieval(df, embedding)
+    df_second = second_retrieval(df_first, embedding)
+    pd.set_option('display.max_colwidth', 20)
+    print(df_second)
+    # df_second.to_csv('.\\result.csv')
+    # print(df_second['link'].to_markdown())
+
+    # df = read_titles_embeddings()
+    # print(df.iloc[0]['title_embed'].dtype)
 
 
     # title = 'A New Hybrid Descent Algorithm for Large-Scale Nonconvex Optimization and Application to Some Image Restoration Problems'
